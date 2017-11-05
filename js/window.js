d91aedef9e94c65f895139bca2e398f224caf874
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2017-11-05 23:28:02 +0100

    Revampped window.js

--- js/window.js
+++ js/window.js
@@ -1,45 +1,78 @@
+var WindowManager; //I want to access it while debugging, will keep it here for now.
 $(document).ready(() => {
-    var Window = {
-        addWindow: function() {
-            var container = $("<div>").addClass("window").draggable({
-                containment: 'body',
-                handle: '.header'
-            }).css({
-                position: 'absolute',
-                top:'calc(100% / 2 - 175px)',
-                left:'calc(100% / 2 - 250px)',
-            }).attr({
-                'id': 'testwindow'
-            });
+    class xWindow {
+        constructor(id, title, content) {
+            this.id = id;
+            this.header = this.buildHeader(title);
+            this.content = this.buildContent(content);
+            this.window = this.buildWindow(id, title, content);
+        }
+        buildHeader(title) {
+            var that = this;
             var closeWindowButton = $("<div>").text('Close').css({
                 'color': 'red',
                 'font-weight': 'bold',
                 'cursor': 'pointer'
             }).attr({
                 'title': 'Close'
-            }).click(function() {
-                container.hide();
+            }).click(function () {
+                that.hide();
             });
             var headerActions = $("<div>").addClass("actions").append(closeWindowButton);
-            var headerText = $("<div>").addClass("text").text("Test Window");
+            var headerText = $("<div>").addClass("text").text(title);
             var header = $("<div>").addClass('header').append(headerText).append(headerActions);
-            var content = $("<div>").addClass('content').text("Well hello there! This is small 'window' example. It can be moved by using the handle (the green part). Click 'close' to close the window. Pretty self explanatory, I guess. Anyhow, enjoy some Lorem Ipsum.");          
-            
-            content.append("<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi facilis mollitia placeat blanditiis omnis, asperiores eveniet nulla. Quae dolore officiis quod blanditiis hic. Qui repellendus natus omnis quae? Iusto, nobis?");
-            content.append("<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi facilis mollitia placeat blanditiis omnis, asperiores eveniet nulla. Quae dolore officiis quod blanditiis hic. Qui repellendus natus omnis quae? Iusto, nobis?");
-            content.append("<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi facilis mollitia placeat blanditiis omnis, asperiores eveniet nulla. Quae dolore officiis quod blanditiis hic. Qui repellendus natus omnis quae? Iusto, nobis?");
-            content.append("<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi facilis mollitia placeat blanditiis omnis, asperiores eveniet nulla. Quae dolore officiis quod blanditiis hic. Qui repellendus natus omnis quae? Iusto, nobis?");
-            content.append("<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi facilis mollitia placeat blanditiis omnis, asperiores eveniet nulla. Quae dolore officiis quod blanditiis hic. Qui repellendus natus omnis quae? Iusto, nobis?");
-            content.append("<br><br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi facilis mollitia placeat blanditiis omnis, asperiores eveniet nulla. Quae dolore officiis quod blanditiis hic. Qui repellendus natus omnis quae? Iusto, nobis?");
-            
-            container.append(header).append(content).hide();
-            
-            $('body').append(container);
+            return header;
+        }
+        buildContent(content) {
+            var content = $("<div>").addClass('content').append(content);
+            return content;
+        }
+        buildWindow(id, title, content) {
+            var container = $("<div>").addClass("window").draggable({
+                containment: 'body',
+                handle: '.header'
+            }).css({
+                position: 'absolute',
+                top: 'calc(100% / 2 - 175px)',
+                left: 'calc(100% / 2 - 250px)',
+            }).attr({
+                'id': id
+            }).append(this.buildHeader(title)).append(this.buildContent(content));
+            return container;
+        }
+        appendContent(content) {
+            console.log(content)
+            this.content.append(content);
+        }
+        toggle() {
+            this.window.toggle();
+        }
+        show() {
+            this.window.show();
+        }
+        hide() {
+            this.window.hide();
+        }
+        init() {
+            $('body').append(this.window);
         }
     }
-    Window.addWindow();
+
+    WindowManager = {
+        initialisedWindows: {},
+        toggle: function (id) { 
+            WindowManager.initialisedWindows[id].toggle();
+        },
+        addWindow: function (id, title, body) {
+            WindowManager.initialisedWindows[id] = new xWindow(id, title, body);
+            WindowManager.initialisedWindows[id].init();
+        }
+    }
+    
+    WindowManager.addWindow('test', 'Hello World', "Well hello there! This is small 'window' example. It can be moved by using the handle (the green part). Click 'close' to close the window. Pretty self explanatory, I guess.");
+
     $('#openWindow').click(() => {
-        $('#testwindow').show();
+        WindowManager.initialisedWindows['test'].show()
     })
 
 });
